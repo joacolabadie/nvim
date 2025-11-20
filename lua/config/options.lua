e439ea01657b16cfc4e1240a0e51d74037c143ca
Joaquin Labadie <labadiejoaco.accs@gmail.com>
2025-11-20 23:13:35 +0000

    chore: update editor options (numbers, tabs, search, clipboard, mouse)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,17 +1,24 @@
--- ui
-vim.opt.termguicolors = true
-vim.opt.cursorline = true
-vim.opt.showmode = false
-
--- clipboard
-vim.opt.clipboard = "unnamedplus"
-
--- indent
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-
--- lines
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.fillchars:append({ vert = " " })
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.mouse = "a"
