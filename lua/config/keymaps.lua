224411a76c066d38f01057b3cf920c9c6d899ac0
Joaquin Labadie <labadiejoaco.accs@gmail.com>
2025-11-21 01:15:20 +0000

    feat: set mapleaders, add convenience keymaps and yank highlight autocmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,28 @@
-local map = vim.keymap.set
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-map("n", "<M-l>", "<Cmd>vertical resize -2<CR>", { desc = "Make window narrower" })
-map("n", "<M-h>", "<Cmd>vertical resize +2<CR>", { desc = "Make window wider" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "x", '"_x')
+
+vim.keymap.set("n", "<C-c>", ":noh<CR>", { silent = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.keymap.set("n", "<leader>sv", "<C-w>v")
+vim.keymap.set("n", "<leader>se", "<C-w>=")
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { silent = true })
+
+vim.keymap.set("n", "<M-l>", "<Cmd>vertical resize -2<CR>")
+vim.keymap.set("n", "<M-h>", "<Cmd>vertical resize +2<CR>")
