07a078a54a12e90dad4c6899b3a1286aed81ef88
Joaquin Labadie <labadiejoaco.accs@gmail.com>
2025-12-05 23:20:27 -0300

    feat: add nvim-web-devicons and set telescope defaults

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,19 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
 	config = function()
+		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
+
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = { "node_modules" },
+			},
+		})
 
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
